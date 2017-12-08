530a8fd1d5214331afa212367328895485b7fa5d
uriel87 <urielch87@gmail.com>
2017-12-08 02:23:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+
+$( document ).ready(function() {
+
+    /* nav links */
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+
+    /* overview car animate */
+    $(".overview").waypoint(function(direction) {
+        $(".js-wp-fadeLeft").addClass("animated fadeInLeft")
+    }, {
+        offset: '80%'
+    });
+
+    function reset($elem) {
+        $elem.before($elem.clone(true));
+        var $newElem = $elem.prev();
+        $elem.remove();
+        return $newElem;
+    }
+
+
+    /* nav mobile */
+    $(".mobile-nav-icon").click(function() {
+
+        var mainNav = $('.main-nav');
+        var fadeInRight = $(".js-wp-fadeInRight");
+        var fadeOutRight = $(".js-wp-fadeOutRight");
+
+        var icon = $('.mobile-nav-icon i');
+
+        if(icon.hasClass("ion-navicon-round")) {
+            console.log("ion-close-round")
+            icon.addClass("ion-close-round");
+            icon.removeClass("ion-navicon-round");
+            mainNav.toggleClass("show-nav")
+        } else {
+            console.log("ion-navicon-round")
+            icon.addClass("ion-navicon-round");
+            icon.removeClass("ion-close-round");
+            mainNav.toggleClass("show-nav")
+        }
+        
+    });
+
+
+});
+
