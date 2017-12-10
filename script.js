3c7a00fc7dc04a992c26071a17a6397da66554fa
uriel87 <urielch87@gmail.com>
2017-12-10 02:15:34 +0200

    change app to run on node

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $( document ).ready(function() {
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top - offset
+                    scrollTop: target.offset().top - 80
                 }, 1000);
                 hideNav()
                 return false;
